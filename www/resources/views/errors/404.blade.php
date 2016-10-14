436eb746f51a7c166336d5efbfb38ec6bd4d79b8
Vladislav Danilchik <danilchican@mail.ru>
2016-10-14 11:15:18 +0300

    Created 404 Error page

---
+++ www/resources/views/errors/404.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    {{ Html::style('css/404/style.css') }}
+</head>
+<body>
+<div class="wrap">
+    <div class="content">
+        <div class="logo">
+            <h1><a href="#">{{ Html::image('images/404/logo.png') }}</a></h1>
+            <span>{{ Html::image('images/404/signal.png') }}Oops! The Page you requested was not found!</span>
+        </div>
+        <div class="buttom">
+            <div class="seach_bar">
+                <p>you can go to <span><a href="{{ URL::previous() }}">home</a></span> page or search here</p>
+                <div class="search_box">
+                    <form>
+                        <input type="text" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}"><input type="submit" value="">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
