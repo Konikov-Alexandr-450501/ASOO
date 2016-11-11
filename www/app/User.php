30a0e9a0cc4826934b4783912efe4e5bea6c30a4
Vladislav Danilchik <danilchican@mail.ru>
2016-11-11 14:40:45 +0300

    Created relation with policy

--- www/app/User.php
+++ www/app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Policy;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,58 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @var index of student
+     */
+    const STUDENT = 1;
+
+    /**
+     * @var index of faculty manager
+     */
+    const FACULTY_MANAGER = 2;
+
+    /**
+     * Returns user relation policy.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function policy() {
+        return $this->belongsTo(Policy::class);
+    }
+
+    /**
+     * Check if a user is Student
+     *
+     * @return bool
+     */
+    public function isStudent() {
+        return $this->policy->id == self::STUDENT;
+    }
+
+    /**
+     * Check if a user is Faculty Manager
+     *
+     * @return bool
+     */
+    public function isFacultyManager() {
+        return $this->policy->id == self::FACULTY_MANAGER;
+    }
+
+    /**
+     * Check user role
+     *
+     * @param string $role
+     * @return bool
+     */
+    public function isRole($role = 'student') {
+        switch($role) {
+            case "student":
+                return $this->isStudent();
+            case "manager":
+                return $this->isFacultyManager();
+            default:
+                return false;
+        }
+    }
 }
