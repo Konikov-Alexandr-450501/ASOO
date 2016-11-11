7800dd22332b3b837495c867e2766798abf2a361
Vladislav Danilchik <danilchican@mail.ru>
2016-11-11 14:15:13 +0300

    Created policies table and PoliciesSeeder

---
+++ www/database/seeds/PoliciesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PoliciesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('policies')->insert([
+            'name' => 'Student'
+        ]);
+        DB::table('policies')->insert([
+            'name' => 'Faculty Manager'
+        ]);
+    }
+}
