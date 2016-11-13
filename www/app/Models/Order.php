5e9457bf5d2a95bf04ae0e9b8bdf8f55127f8e1c
Vladislav Danilchik <danilchican@mail.ru>
2016-11-13 15:26:52 +0300

    Created relations for Order

--- www/app/Models/Order.php
+++ www/app/Models/Order.php
@@ -14,6 +14,24 @@ class Order extends Model
      * @var array
      */
     protected $fillable = [
-        'description', 'body', 'status', 'faculty_id', 'type_id', 'user_id'
+        'description', 'body', 'status', 'type_id', 'user_id'
     ];
+
+    /**
+     * Returns the owner of the Order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function owner(){
+        return $this->hasOne(User::class);
+    }
+
+    /**
+     * Returns the type of Order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function type(){
+        return $this->hasOne(Type::class);
+    }
 }
