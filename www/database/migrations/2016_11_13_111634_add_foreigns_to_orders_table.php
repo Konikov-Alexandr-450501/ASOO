05d5a30757c4de7a3cd1e662f724af258337dda2
Vladislav Danilchik <danilchican@mail.ru>
2016-11-13 14:25:09 +0300

    Added foreigh keys to orders table

---
+++ www/database/migrations/2016_11_13_111634_add_foreigns_to_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignsToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('faculty_id')->unsigned();
+            $table->integer('type_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+
+            $table->foreign('faculty_id')
+                ->references('id')->on('faculties')
+                ->onDelete('cascade');
+            $table->foreign('type_id')
+                ->references('id')->on('types')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropForeign('orders_faculty_id_foreign');
+            $table->dropForeign('orders_type_id_foreign');
+            $table->dropForeign('users_user_id_foreign');
+
+            $table->dropColumn(['faculty_id', 'type_id', 'user_id']);
+        });
+    }
+}
