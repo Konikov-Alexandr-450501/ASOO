673ec6a7831f3cf8becde8a5b78c2dd4495a05e9
Vladislav Danilchik <danilchican@mail.ru>
2016-11-15 10:50:56 +0300

    Changed index view

--- www/resources/views/partials/orders/index.blade.php
+++ www/resources/views/partials/orders/index.blade.php
@@ -1,26 +1,29 @@
 <template id="order-list">
     <table class="table">
         <thead>
-        <tr>
-            <th>#</th>
-            <th>Description</th>
-            <th>Type <i class="fa fa-sort" aria-hidden="true"></i></th>
-            <th>Created at <i class="fa fa-sort" aria-hidden="true"></i></th>
-            <th>Status <i class="fa fa-sort" aria-hidden="true"></i></th>
-            <th>Action</th>
-        </tr>
+            <tr>
+                <th>#</th>
+                <th>Description</th>
+                <th>Type <i class="fa fa-sort" aria-hidden="true"></i></th>
+                <th>Created at <i class="fa fa-sort" aria-hidden="true"></i></th>
+                <th>Status <i class="fa fa-sort" aria-hidden="true"></i></th>
+                <th>Action</th>
+            </tr>
         </thead>
         <tbody>
-        <tr v-for="order in list">
-            <td>#@{{ order.id }}</td>
-            <td>@{{ order.description }}</td>
-            <td>@{{ order.type_id }}</td>
-            <td>@{{ order.created_at }}</td>
-            <td>@{{ order.status }}</td>
-            <td><button class=""><i class="fa fa-trash" aria-hidden="true"></i></button></td>
-        </tr>
+            <tr v-for="order in list">
+                <td>#@{{ order.id }}</td>
+                <td>@{{ order.description }}</td>
+                <td>
+                    <div v-if="order.type_id == 1">Sheet</div>
+                    <div v-else>Reference</div>
+                </td>
+                <td>@{{ order.created_at }}</td>
+                <td>@{{ order.status }}</td>
+                <td><button class=""><i class="fa fa-trash" aria-hidden="true"></i></button></td>
+            </tr>
         </tbody>
-    </table>
+     </table>
 </template>
 
 <orders-comp></orders-comp>
