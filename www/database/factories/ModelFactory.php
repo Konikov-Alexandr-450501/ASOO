570729df3d042e118d0acbc700b83570bb1676a9
danilchican <danilchican@mail.ru>
2016-11-25 13:49:53 +0300

    Created orders seeder

--- www/database/factories/ModelFactory.php
+++ www/database/factories/ModelFactory.php
@@ -22,3 +22,14 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Models\Order::class, function (Faker\Generator $faker) {
+
+    return [
+        'description' => $faker->realText(80),
+        'body'        => $faker->realText(200),
+        'status'      => $faker->numberBetween(0, 1),
+        'user_id'     => 21,
+        'type_id'     => $faker->numberBetween(1, 2),
+    ];
+});
