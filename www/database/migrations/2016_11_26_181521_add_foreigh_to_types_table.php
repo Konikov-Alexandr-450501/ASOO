328c303999b4eb72bb97aeb9fb37ac34421f0c7f
danilchican <danilchican@mail.ru>
2016-11-27 15:18:12 +0300

    Added relation for type

---
+++ www/database/migrations/2016_11_26_181521_add_foreigh_to_types_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeighToTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        Schema::table('types', function (Blueprint $table) {
+            $table->integer('kind_id')->unsigned();
+
+            $table->foreign('kind_id')
+                ->references('id')->on('kinds')
+                ->onDelete('cascade');
+        });
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('types', function (Blueprint $table) {
+            $table->dropForeign('types_kind_id_foreign');
+            $table->dropColumn(['kind_id']);
+        });
+    }
+}
